b9953b82a82469193bcc70b2b351f9346f147952
theo <bernardon.theo@gmail.com>
2025-06-10 14:05:29 +0200

    feat: ajout d'un fichier de personas et mise à jour du script pour gérer les modifications des personas et du numéro de projet

--- resources/issue_template/Projet_number_edit.py
+++ resources/issue_template/Projet_number_edit.py
@@ -1,23 +1,36 @@
 import os
 import yaml
 
-NUMBER_PROJET = input("Entrez le n° du projet : ")
 DIR = ".github\\ISSUE_TEMPLATE"
 
 
-def edit_yaml_files(number_projet, directory=DIR):
-    yaml_files = [f for f in os.listdir(directory) if f.endswith((".yaml", ".yml"))]
+def edit_yaml_files(number_projet, personas=None, directory=DIR):
+    yaml_files = [
+        f
+        for f in os.listdir(directory)
+        if f.endswith((".yaml", ".yml")) & (not f.startswith("."))
+    ]
 
     # itération sur les fichiers YAML
     for filename in yaml_files:
         filepath = os.path.join(directory, filename)
+        print(f"Modification du fichier : {filename}")
 
         # Import du fichier Yaml
         with open(filepath, "r", encoding="utf-8") as file:
             data = yaml.safe_load(file)
 
         # Modification du nombre du projet
-        data["projects"] = data["projects"].replace("Projet_number", str(number_projet))
+        if bool(number_projet):
+            data["projects"] = data["projects"].replace(
+                "Projet_number", str(number_projet)
+            )
+
+        # Modification des personas
+        if bool(personas):
+            if "options" in data["body"][0]["attributes"].keys():
+                data["body"][0]["attributes"]["options"] = personas
+                print("Personas ajoutées.")
 
         # save yml
         with open(filepath, "w", encoding="utf-8") as file:
@@ -25,4 +38,22 @@ def edit_yaml_files(number_projet, directory=DIR):
 
 
 if __name__ == "__main__":
-    edit_yaml_files(NUMBER_PROJET)
+    print()
+    EDIT = input("Que voulez vous éditer (projet / personas / all) : ")
+
+    # Demande du numéro de projet
+    if EDIT == "projet" or EDIT == "all":
+        print()
+        NUMBER_PROJET = input("Entrez le n° du projet : ")
+    else:
+        NUMBER_PROJET = None
+
+    # Récupération des personas
+    if EDIT == "personas" or EDIT == "all":
+        # Import du fichier Yaml
+        with open(DIR + "\\.personas.yml", "r", encoding="utf-8") as file:
+            PERSONAS = yaml.safe_load(file)["personas"]
+    else:
+        PERSONAS = None
+
+    edit_yaml_files(NUMBER_PROJET, PERSONAS)
